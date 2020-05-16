7720026fe5ff9be3dd96e543cfd56e7a53f72fbd
snamiki1212 <snamiki1212@gmail.com>
2020-05-16 18:21:08 +0900

    Update

--- index.ts
+++ index.ts
@@ -1,18 +1,29 @@
-const tweets = require("./backup/data/tweet");
-const { Parser } = require("json2csv");
+import { Parser } from "json2csv";
 import { writeFile } from "fs";
-import {config} from './config'
+import { config } from "./config";
+
+const tweets = require("./backup/data/tweet");
 
 const outputPath = "./output.csv";
-const excludingIDs = config.excludingIDs
+const excludingIDs = config.excludingIDs;
 const squash = (list: any[]) => list.map((obj) => obj.tweet);
 const addCustomFields = (list: any[]) =>
   list.map((it: any) => {
     const hasAtMark = it.full_text.includes("@");
     const hasHTTP = it.full_text.includes("http");
     const isMoreThan139 = Array.from(it.full_text).length >= 139;
-    const isExcludedID = excludingIDs.includes(Number(it.id))
-    return Object.assign(it, { hasAtMark, hasHTTP, isMoreThan139, isExcludedID });
+    const isExcludedID = excludingIDs.includes(Number(it.id));
+    const created_at_unixtime = new Date(it.created_at).getTime() / 1000; // REF: https://stackoverflow.com/questions/11893083/convert-normal-date-to-unix-timestamp
+    const SHOULD_VIEW =
+      !hasAtMark && !hasHTTP && isMoreThan139 && !isExcludedID;
+    return Object.assign(it, {
+      created_at_unixtime,
+      hasAtMark,
+      hasHTTP,
+      isMoreThan139,
+      isExcludedID,
+      SHOULD_VIEW,
+    });
   });
 
 // TODO: 動的に取りたい
@@ -31,10 +42,11 @@ const fields = [
   "full_text",
   "lang",
   // custom fields
+  "created_at_unixtime",
   "isMoreThan139",
   "hasAtMark",
   "hasHTTP",
-  "isExcludedID"
+  "isExcludedID",
 ];
 const options = { fields };
 
